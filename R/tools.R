7a4d2b5f266a557d9a65b4b8e34958624f649182
jason@ubuntu <lijxug@gmail.com>
2020-12-06 15:04:26 +0800

    specify the dependencies

--- R/tools.R
+++ R/tools.R
@@ -202,5 +202,5 @@ EXWB.writeSheet = function(wb, sheetName, sheetData, overwrite = F, ...){
 #' @export
 gg_color_hue <- function(n) {
   hues = seq(15, 375, length = n + 1)
-  hcl(h = hues, l = 65, c = 100)[1:n]
+  grDevices::hcl(h = hues, l = 65, c = 100)[1:n]
 }
