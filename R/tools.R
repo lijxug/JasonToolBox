f46aef00078b40b2bac05285fa2016a6691b9bae
Jason <lijxug@gmail.com>
2023-07-07 11:21:04 +0800

    add functions

--- R/tools.R
+++ R/tools.R
@@ -1,3 +1,8 @@
+#' @import methods
+#' @import stats
+#' @import utils 
+NULL
+
 #' Get the lower triangle of a symmetric matrix
 #'
 #' @param x_mt A symmetric matrix
@@ -198,6 +203,7 @@ EXWB.initiate = function(...) {
 #' Sheet name must have <= 31 characters without any strange characters like !\@\#\$\%\^\&\*\(\)
 #' @param sheetData Data frame or tibble to write.
 #' @param overwrite Logical; if to overwrite the sheet with the same name.
+#' @param ... Other arguments for openxlsx::writeDataTable
 #'
 #' @export
 #'
@@ -218,7 +224,7 @@ EXWB.writeSheet = function(wb, sheetName, sheetData, overwrite = F, ...){
   }
   openxlsx::addWorksheet(wb, sheetName = sheetName, gridLines = T)
   # sheetData = sheetData %>% mutate_if(is.double, round, digits = 4)
-  openxlsx::writeDataTable(wb, sheet = sheetName, x = sheetData, colNames = TRUE, rowNames = TRUE,tableStyle = "TableStyleLight9")
+  openxlsx::writeDataTable(wb, sheet = sheetName, x = sheetData, colNames = TRUE, rowNames = TRUE,tableStyle = "TableStyleLight9", ...)
   invisible(wb)
 }
 
@@ -235,10 +241,11 @@ gg_color_hue <- function(n) {
 
 #' Differential expression analysis using Wilcoxon test
 #'
-#'@param x A list with two named expression matrixces of n genes * m cells.
-#'@param ... Other options. Placeholder, not used yet.
-#'
-#'@export
+#' @param x A list with two named expression matrixces of n genes * m cells.
+#' @param ... Other options. Placeholder, not used yet.
+#' 
+#' @return a data.frame
+#' @export
 dea.wilcox = function(x, ...) {
   # x being the list with two named expression matrixes,
   # ... whose rownames correspond to tx_names and colnames to sample_names
@@ -258,7 +265,7 @@ dea.wilcox = function(x, ...) {
   iter01 = ".I1_fun"
   initiatePB(iter01)
   for (i in 1:n_tx) {
-    t_res = wilcox.test(mt1[i,], mt2[i,])
+    t_res = stats::wilcox.test(mt1[i,], mt2[i,])
     p_vals[i] = t_res$p.value
     means_mt1[i] = mean(mt1[i, ])
     means_mt2[i] = mean(mt2[i, ])
@@ -267,9 +274,9 @@ dea.wilcox = function(x, ...) {
   }
   FC = means_mt2 / (means_mt1 + .0001)
   LFC = log2(FC)
-  p_adjs = p.adjust(p_vals)
+  p_adjs = stats::p.adjust(p_vals)
 
-  res_tbl = tibble(
+  res_tbl = data.frame(
     id = rownames(mt1),
     means_mt1 = means_mt1,
     means_mt2 = means_mt2,
@@ -308,4 +315,211 @@ apply_MM <- function(X, MARGIN = 1, FUN) {
   }
   res[as.integer(names(tmp)) + 1] <- tmp
   res
+}
+
+#' Function for permutation test
+#' 
+#' @param x,y numeric vector of data values. Non-finite values will be ommitted
+#' @param n_perm permutation times
+#' @param alternative a character string specifying the alternative hypothesis, must be one of "two.sided" (default), "greater" or "less". You can specify just the initial letter.
+#' @param verbose logical value specify whether to print logs. Default = TRUE
+#' 
+#' @return A named list
+#' @export
+permu.test = function(x, y, n_perm = 2000, alternative = 'two.sided', verbose = T){
+  x = x[is.finite(x)]
+  y = y[is.finite(y)]
+  observed = median(x) - median(y)
+  all_values = c(x, y)
+  if(verbose){
+    iFUN = pbmcapply::pbmclapply
+  } else {
+    iFUN = parallel::mclapply
+  }
+  perm_dist = unlist(iFUN(mc.cores = 50, 1:n_perm, function(x){
+    perm_x = sample(all_values, size = length(x))
+    perm_y = sample(all_values, size = length(y))
+    return(median(perm_x) - median(perm_y))
+  }))
+  
+  emp_p_g = (sum(perm_dist >= observed)+1)/(n_perm + 1)
+  emp_p_l = (sum(perm_dist <= observed)+1)/(n_perm + 1)
+  emp_p_b = min(emp_p_g, emp_p_l)*2
+  if(alternative == 'greater'){
+    return(list(observe = observed, dist = perm_dist, p.value = emp_p_g, n_perm = n_perm, alternative = alternative))
+  } else if(alternative == 'less') {
+    return(list(observe = observed, dist = perm_dist, p.value = emp_p_l, n_perm = n_perm, alternative = alternative))
+  } else if(alternative == 'one.sided'){
+    return(list(observe = observed, dist = perm_dist, p.value = min(emp_p_g,emp_p_l), n_perm = n_perm, alternative = alternative))
+  } else {
+    return(list(observe = observed, dist = perm_dist, p.value = emp_p_b, n_perm = n_perm, alternative = alternative))
+  }
+}
+
+
+
+#' Wrapper for batch testing
+#' 
+#' @param data_df The data frame to test
+#' @param group_var The variable name for testing groups
+#' @param test_var The variable name for testing values 
+#' @param test_set A 2 x n combn matrix for pairs of condition to test
+#' @param by_facet A string for the facet
+#' @param method One of the c('permutation', <to be added>)
+#' @param pairby The variable name for pairing, default NULL
+#' @param p_cutoff Specify the cutoff for significant p values
+#' @param ... Other parameters for test functions
+#' 
+#' 
+#' @importFrom magrittr `%>%`
+#' @return A data.frame
+#' @export
+batch_testing = function(data_df,
+                         group_var,
+                         test_var,
+                         test_set,
+                         by_facet = NULL,
+                         method = 'permutation',
+                         pairby = NULL,
+                         p_cutoff = 0.1,
+                         ...) {
+  
+  
+  res_tbl = dplyr::tibble()
+  args = list(...)
+  args$alternative = ifelse(is.null(args$alternative), 'two.sided', args$alternative)
+  args$verbose = ifelse(is.null(args$verbose), T, args$verbose)
+  args$n_perm = ifelse(is.null(args$n_perm), 2000, args$n_perm)
+  args$paired = ifelse(is.null(pairby), F, T)
+  
+  if (!is.null(by_facet)) {
+    facets2test = unique(data_df[[by_facet]])
+    facets2test = facets2test[!is.na(facets2test)]
+  } else {
+    facets2test = '.all'
+  }
+  for (i_facet in facets2test) {
+    if(i_facet == '.all'){
+      t_test_tbl = data_df
+    } else {
+      t_test_tbl = data_df[data_df[[by_facet]] == i_facet, ]
+    }
+    
+    for (i_col in 1:ncol(test_set)) {
+      # Prepare data
+      x_cat = test_set[1, i_col]
+      y_cat = test_set[2, i_col]
+      
+      if (args$paired) {
+        if (any(!c(x_cat, y_cat) %in% t_test_tbl[[group_var]])) {
+          x1 = NULL
+          x2 = NULL
+        } else{
+          p_test_tbl = t_test_tbl %>%
+            dplyr::filter((!!dplyr::sym(group_var)) %in% c(x_cat, y_cat)) %>%
+            tidyr::pivot_wider(
+              id_cols = dplyr::all_of(pairby),
+              names_from = group_var,
+              values_from = test_var
+            ) %>% dplyr::filter(!is.na(!!(dplyr::sym(x_cat))) &
+                                  !is.na(!!(dplyr::sym(y_cat))))
+          x1 = p_test_tbl[[x_cat]]
+          x2 = p_test_tbl[[y_cat]]
+        }
+      } else {
+        x1 = t_test_tbl[t_test_tbl[[group_var]] == x_cat, test_var, drop = T]
+        if (y_cat == 'others') {
+          x2 = t_test_tbl[t_test_tbl[[group_var]] != x_cat, test_var, drop = T]
+        } else {
+          x2 = t_test_tbl[t_test_tbl[[group_var]] == y_cat, test_var, drop = T]
+        }
+      }
+      
+      x1 = x1[is.finite(x1)]
+      x2 = x2[is.finite(x2)]
+      
+      if (length(x1) <2 || length(x2) < 2) {
+        next
+      }
+      if (length(x1) == 2 || length(x2) ==2){
+        warning('Values too few, using permutation test.\n')
+        test_res = permu.test(x1, x2, ...)
+      } else {
+        # Test
+        if (method == 'permutation') {
+          test_res = permu.test(x1, x2, ...)
+        } else if (method == 'wilcox') {
+          test_res = wilcox.test(x1, x2, paired = args$paired, ...)
+        } else if (method == 'ttest') {
+          # Test for normality assumption
+          if (args$paired) {
+            if (length(x2 - x1) < 3) {
+              shape_res = list(p.value = 0)
+            } else if (length(x2 - x1) < 50) {
+              shape_res = tryCatch({
+                shapiro.test(x2 - x1)
+              }, error = function(e) {
+                print(e)
+                return(list(p.value = 0))
+              })
+            } else {
+              shape_res = ks.test(x2 - x1,
+                                  pnorm,
+                                  mean = mean(x2 - x1),
+                                  sd = sd(x2 - x1))
+            }
+            
+          } else {
+            if (length(x1) < 3) {
+              shape_res1 = list(p.value = 0)
+            } else if (length(x1) < 50) {
+              shape_res1 = shapiro.test(x1)
+            } else {
+              shape_res1 = ks.test(x1, pnorm, mean = mean(x1), sd = sd(x1))
+            }
+            
+            if (length(x2) < 3) {
+              shape_res2 = list(p.value = 0)
+            } else if (length(x2) < 50) {
+              shape_res2 = shapiro.test(x2)
+            } else {
+              shape_res2 = ks.test(x2, pnorm, mean = mean(x2), sd = sd(x2))
+            }
+            shape_res = list(p.value = min(shape_res1$p.value, shape_res2$p.value))
+          }
+          
+          if (shape_res$p.value > p_cutoff) {
+            test_res = t.test(x1, x2, paired = args$paired, ...)
+          } else {
+            # Not applicable
+            test_res = list(p.value = NA)
+          }
+        } else {
+          warning('Not supported yet. Falling back to permutation test.\n')
+          test_res = permu.test(x1,
+                                x2,
+                                n_perm = 10000,
+                                alternative = args$alternative)
+        }
+      }
+      
+      # Arrange position
+      res_tbl = dplyr::bind_rows(
+        res_tbl,
+        data.frame(
+          facets = i_facet,
+          y_position = max(t_test_tbl[[test_var]], na.rm = T) * (1 + i_col * 0.1),
+          xmin = x_cat,
+          xmax = y_cat,
+          p.value = test_res$p.value, 
+          grp1_mean = mean(x1, na.rm = T),
+          grp2_mean = mean(x2, na.rm = T),
+          grp1_zscore = mean((x1-mean(c(x1,x2)))/sd(c(x1,x2))),
+          grp2_zscore = mean((x2-mean(c(x1,x2)))/sd(c(x1,x2)))
+        )
+      )
+    }
+  }
+  
+  return(res_tbl)
 }
